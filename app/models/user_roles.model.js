6cefa322b31530517843cb9d978ebcda13174a1e
Ghs267 <hariyantosetiadi@gmail.com>
2021-04-28 21:33:15 +0700

    add new file

---
+++ app/models/user_roles.model.js
@@ -0,0 +1,16 @@
+// module.exports=(sequelize,Sequelize)=>{
+//     const userRole = sequelize.define("user_roles",{
+//         userId:{
+//             type: Sequelize.INTEGER,
+//             primaryKey:true
+//         },
+//         roleId:{
+//             type: Sequelize.INTEGER,
+//             primaryKey:true
+//         },
+//         link:{
+//             type: Sequelize.STRING
+//         }
+//     })
+//     return userRole;
+// }
