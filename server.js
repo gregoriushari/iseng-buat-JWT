441661fb2b1043d019c29bdea10a2737d5a1851c
Ghs267 <hariyantosetiadi@gmail.com>
2021-04-28 21:32:29 +0700

    edit server.js

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(bodyParser.json())
 //parse req of content type -application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}))
 
+const db = require("./app/models")
+const Role = db.role
+
+db.sequelize.sync()
+// db.sequelize.sync({force:true}).then(()=>{
+//     console.log("drop and resync db")
+//     initial()
+// })
+
 //simple route
 app.get("/",(req,res)=>{
     res.json({message:"Welcome to API with JWT "})
@@ -26,3 +35,20 @@ const PORT = process.env.PORT ||8000
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}.`)
 })
+
+// function initial(){
+//     Role.create({
+//         id: 1,
+//         name: "user"
+//     })
+     
+//     Role.create({
+//         id: 2,
+//         name: "moderator"
+//     })
+     
+//     Role.create({
+//         id: 3,
+//         name: "admin"
+//     })
+// }
