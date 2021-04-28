bf52eb49da8241015e6b95eb488254670849efae
Ghs267 <hariyantosetiadi@gmail.com>
2021-04-28 21:31:18 +0700

    edit index.js

---
+++ app/models/index.js
@@ -0,0 +1,46 @@
+const config = require("../config/db.config")
+
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+        host: config.HOST,
+        dialect: config.dialect,
+        operatorsAliases:false,
+
+        pool:{
+            max:config.pool.max,
+            min:config.pool.min,
+            acquire:config.pool.acquire,
+            idle :config.pool.idle
+        }
+    }
+)
+
+const db = {}
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+db.user = require("../models/user.model")(sequelize,Sequelize)
+db.role = require("../models/role.model")(sequelize,Sequelize)
+// db.user_roles = require("../models/user_roles.model")(sequelize,Sequelize)
+
+db.role.belongsToMany(db.user,{
+    through: "user_roles",
+    as:"user",
+    foreignKey : "roleId",
+})
+
+db.user.belongsToMany(db.role,{
+    through: "user_roles",
+    as:"role",
+    foreignKey : "userId",
+})
+
+db.ROLES = ["user","admin","moderator"]
+
+
+module.exports = db
