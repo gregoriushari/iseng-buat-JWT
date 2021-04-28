28293f402e7bfbdc82965192a57831ebc84dd36b
Ghs267 <hariyantosetiadi@gmail.com>
2021-04-28 15:10:08 +0700

    add server js

---
+++ server.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+let corsOptions = {
+    origin:"http://localhost:8081"
+}
+
+app.use(cors(corsOptions))
+
+//parse req of content type - application/json
+app.use(bodyParser.json())
+
+//parse req of content type -application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+
+//simple route
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to API with JWT "})
+})
+
+//set port yang digunakan 
+const PORT = process.env.PORT ||8000
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}.`)
+})
