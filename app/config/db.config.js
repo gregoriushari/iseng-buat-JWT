2ca4bddd9f6c0932d91cce69c1db75e72dcf251f
Ghs267 <hariyantosetiadi@gmail.com>
2021-04-28 15:09:26 +0700

    add app file

---
+++ app/config/db.config.js
@@ -0,0 +1,22 @@
+module.exports={
+    HOST:"localhost",
+    USER:"root",
+    PASSWORD:"",
+    DB:"apiJWT",
+    dialect:"mysql",
+    pool :{
+        max:5,
+        min:0,
+        acquire:3000,
+        idle:1000
+    }
+}
+
+/*
+    max: maximum number of connection in pool
+    min: minimum number of connection in pool
+    idle: maximum time, in milliseconds, 
+    that a connection can be idle before being released
+    acquire: maximum time, in milliseconds, 
+    that pool will try to get connection before throwing error
+*/
