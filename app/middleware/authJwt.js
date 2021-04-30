fc6f2bb8c808e1bf0eb838914cc02e0d9ee0aff4
Ghs267 <hariyantosetiadi@gmail.com>
2021-04-30 20:47:39 +0700

    edit middleware

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -11,4 +11,78 @@ verifyToken = (req,res, next)=>{
             message: "No token provided"
         })
     }
+    jwt.verify(token, config.secret, (err, decoded)=>{
+        if(err){
+            return res.status(401).send({
+                message: "Unauthorized"
+            })
+        }
+        req.userId = decoded.id
+        next()
+    })
 }
+
+isAdmin = (req, res, next)=>{
+    User.findByPk(req.userId).then(user=>{
+        user.getRoles().then(roles=>{
+            for(let i=0; i< roles.length; i++){
+                if(roles[i].name==="admin"){
+                    next()
+                    return
+                }
+            }
+            res.status(403).send({
+                message: "Khusus role admin"
+            })
+            return
+        })
+    })
+}
+
+isModerator = (req, res, next)=>{
+    User.findByPk(req.userId).then(user=>{
+        user.getRoles().then(roles=>{
+            for(let i=0; i< roles.length; i++){
+                if(roles[i].name==="moderator"){
+                    next()
+                    return
+                }
+            }
+            res.status(403).send({
+                message: "Khusus role moderator"
+            })
+            return
+        })
+    })
+}
+isModeratorOrAdmin = (req, res, next)=>{
+    User.findByPk(req.userId).then(user=>{
+        user.getRoles().then(roles=>{
+            for(let i=0; i< roles.length; i++){
+                if(roles[i].name==="moderator"){
+                    next()
+                    return
+                }
+
+                if(roles[i].name==="admin"){
+                    next()
+                    return
+                }
+            }
+            res.status(403).send({
+                message: "Khusus role moderator atau admin"
+            })
+            return
+        })
+    })
+}
+
+const authJwt ={
+    verifyToken: verifyToken,
+    isAdmin: isAdmin,
+    isModerator: isModerator,
+    isModeratorOrAdmin: isModeratorOrAdmin
+}
+
+module.exports= authJwt
+
--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -21,7 +21,7 @@ checkDuplicateUsernameOrEmail = (req, res, next)=>{
             }
         }).then(user=>{
             if(user){
-                res.status.send({
+                res.status(400).send({
                     message:"Gagal, Email sudah digunakan"
                 })
                 return
@@ -33,7 +33,7 @@ checkDuplicateUsernameOrEmail = (req, res, next)=>{
 
 checkRolesExisted=(req,res,next)=>{
     if(req.body.roles){
-        for(let i=0;i<ROLES.length; i++){
+        for(let i=0;i<req.body.roles.length; i++){
             if (!ROLES.includes(req.body.roles[i])) {
                 res.status(400).send({
                   message: "Failed! Role does not exist = " + req.body.roles[i]
